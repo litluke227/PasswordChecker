98d4f915c87ff11cb999e488e247fd5acedd13c8
litluke227 <lucasm.lm95@gmail.com>
2025-02-12 10:28:45 -0500

    caught string errors

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner password = new Scanner(System.in);
         System.out.println("Please create a password");
-        int correctPW = password.nextInt();
+        int correctPW = 0;
+        String trash = "";
+        if(password.hasNextInt())
+        {
+            correctPW = password.nextInt();
+            password.nextLine();
+        }
+        else{
+            trash = password.nextLine();
+            System.out.println("Enter a valid number, not"+ trash);
+        }
         int pW = 0;
         while (pW != correctPW) {
             Scanner checkPW = new Scanner(System.in);
             System.out.println("Please enter your password");
-            pW = checkPW.nextInt();
+            if(checkPW.hasNextInt())
+            {
+                pW = checkPW.nextInt();
+                checkPW.nextLine();
+            }
+            else{
+                trash = checkPW.nextLine();
+                System.out.println("Enter a valid number, not "+ trash);
+            }
         }
         System.out.println("Correct Password");
     }
